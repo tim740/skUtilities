08111059fc8af7c269b4380ba6ca2420b78911de
tim740 <tim740x@gmail.com>
2016-03-05 14:28:35 +0000

    Version

--- src/uk/tim740/skUtilities/Main.java
+++ src/uk/tim740/skUtilities/Main.java
@@ -7,16 +7,17 @@ import uk.tim740.skUtilities.convert.RegConvert;
 import uk.tim740.skUtilities.util.RegUtil;
 
 public class Main extends JavaPlugin {
+    private static String ver = Bukkit.getServer().getPluginManager().getPlugin("skUtilities").getDescription().getVersion();
 
 	@Override
 	public void onEnable() {
         Skript.registerAddon(this);
         RegConvert.regC();
         RegUtil.regU();
-        Bukkit.getServer().getLogger().info("[skUtilities] v" + Bukkit.getServer().getPluginManager().getPlugin("skUtilities").getDescription().getVersion() + " has fully loaded!");
+        Bukkit.getServer().getLogger().info("[skUtilities] v" + ver + " has fully loaded!");
     }
     public static void prErr(String s, String c) {
-        Bukkit.getServer().getLogger().severe("[skUtilities] " + s + " ("+ c +")");
-        Bukkit.broadcast("[skUtilities - ERROR] " + s + " ("+ c +")", "skUtilities.error");
+        Bukkit.getServer().getLogger().severe("[skUtilities] v" + ver + ": "  + s + " ("+ c +")");
+        Bukkit.broadcast("(ERROR) [skUtilities] v" + ver + ": " + s + " ("+ c +")", "skUtilities.error");
     }
 }
