a1a35057785f1fe62b6a0b2530c70fcfa0919b2e
tim740 <tim740x@gmail.com>
2016-03-15 23:15:37 +0000

    optimise

--- src/uk/tim740/skUtilities/util/EffPacketTrial.java
+++ src/uk/tim740/skUtilities/util/EffPacketTrial.java
@@ -15,30 +15,27 @@ import ch.njol.util.Kleenean;
 import uk.tim740.skUtilities.skUtilities;
 
 /**
- * Made by @Sashie - Tweaked and Optimised by @tim740
+ * Made by @Sashie - Tweaked by @tim740
  */
 public class EffPacketTrial extends Effect {
 	private Expression<Player> player;
 	@Override
 	protected void execute(@Nullable Event e) {
-            String p = Bukkit.getServer().getClass().getPackage().getName();
-            String ver = p.substring(p.lastIndexOf(".") + 1, p.length());
-            try {
-                Class<?> craftPlayer = Class.forName("org.bukkit.craftbukkit." + ver + ".entity.CraftPlayer");
-                Class<?> PacketPlayOutGameStateChange = Class.forName("net.minecraft.server." + ver + ".PacketPlayOutGameStateChange");
-                Class<?> Packet = Class.forName("net.minecraft.server." + ver + ".Packet");
-                Constructor<?> playOutConstructor = PacketPlayOutGameStateChange.getConstructor(Integer.TYPE, Float.TYPE);
-                Object packet = playOutConstructor.newInstance(5, 0);
-                Object craftPlayerObject = craftPlayer.cast(player.getSingle(e));
-                Method getHandleMethod = craftPlayer.getMethod("getHandle", (Class<?>[])new Class[0]);
-                Object handle = getHandleMethod.invoke(craftPlayerObject);
-                Object pc = handle.getClass().getField("playerConnection").get(handle);
-                Method sPM = pc.getClass().getMethod("sendPacket", Packet);
-                sPM.invoke(pc, packet);
-            }
-            catch (Exception ex) {
-                skUtilities.prErr("Player didn't have a compatible version of Minecraft!", getClass().getSimpleName(), 1);
-            }
+        String p = Bukkit.getServer().getClass().getPackage().getName();
+        String ver = p.substring(p.lastIndexOf(".") + 1, p.length());
+        try {
+            Class<?> cPlayer = Class.forName("org.bukkit.craftbukkit." + ver + ".entity.CraftPlayer");
+            Class<?> PacketPlayOutGameStateChange = Class.forName("net.minecraft.server." + ver + ".PacketPlayOutGameStateChange");
+            Constructor<?> playOutConstructor = PacketPlayOutGameStateChange.getConstructor(Integer.TYPE, Float.TYPE);
+            Method getHandleMethod = cPlayer.getMethod("getHandle", (Class<?>[])new Class[0]);
+            Object handle = getHandleMethod.invoke(cPlayer.cast(player.getSingle(e)));
+            Object pc = handle.getClass().getField("playerConnection").get(handle);
+            Method sPM = pc.getClass().getMethod("sendPacket", Class.forName("net.minecraft.server." + ver + ".Packet"));
+            sPM.invoke(pc, playOutConstructor.newInstance(5, 0));
+        }
+        catch (Exception ex) {
+            skUtilities.prErr("Player didn't have a compatible version of Minecraft!", getClass().getSimpleName(), 1);
+        }
 	}
 
     @SuppressWarnings("unchecked")
