1fe21242bf9120ccdb22962eae8d1117e8a3aeca
tim740 <tim740x@gmail.com>
2016-03-21 16:43:51 +0000

    File Last Modified

--- src/uk/tim740/skUtilities/RegFiles.java
+++ src/uk/tim740/skUtilities/RegFiles.java
@@ -15,6 +15,7 @@ class RegFiles {
         Skript.registerExpression(ExprDirList.class,String.class,ExpressionType.PROPERTY,"files in dir[ectory] %string%", "dir[ectory] %string%'s files");
         Skript.registerExpression(ExprReadLine.class,String.class,ExpressionType.PROPERTY,"line %number% in file %string%", "file %string%'s line %number%");
         Skript.registerExpression(ExprFileContents.class,String.class,ExpressionType.PROPERTY,"file contents of %string%", "%string%'s file contents");
+        Skript.registerExpression(ExprFileLastModified.class,Number.class,ExpressionType.PROPERTY,"file %string%'s last modified value", "last modified value of file %string%");
 
         Skript.registerExpression(SExprFileAttribute.class,Boolean.class,ExpressionType.PROPERTY,"(0¦readable|1¦writable|2¦hidden) attribute of file %string%", "file %string%'s (0¦readable|1¦writable|2¦hidden) attribute");
 
