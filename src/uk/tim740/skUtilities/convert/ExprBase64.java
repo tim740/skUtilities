e488495f0e0308f5cbaed3b6930b0e3db3bbd679
tim740 <tim740x@gmail.com>
2016-08-26 19:44:07 +0100

    rename

--- src/uk/tim740/skUtilities/convert/ExprBase64.java
+++ src/uk/tim740/skUtilities/convert/ExprBase64.java
@@ -1,28 +1,27 @@
 package uk.tim740.skUtilities.convert;
 
-import javax.annotation.Nullable;
-import javax.xml.bind.DatatypeConverter;
-import java.util.Base64;
-import java.nio.charset.StandardCharsets;
-
-import org.bukkit.event.Event;
-
 import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+
+import javax.annotation.Nullable;
+import javax.xml.bind.DatatypeConverter;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 /**
  * Created by tim740 on 07/03/16
  */
 public class ExprBase64 extends SimpleExpression<String> {
 	private Expression<String> b64;
-    private int b64Ty;
+    private int ty;
 
 	@Override
 	@Nullable
 	protected String[] get(Event arg0) {
-        if (b64Ty == 0) {
+        if (ty == 0) {
             return new String[]{Base64.getEncoder().encodeToString(b64.getSingle(arg0).getBytes(StandardCharsets.UTF_8))};
         }else{
             return new String[]{new String(DatatypeConverter.parseBase64Binary(b64.getSingle(arg0)), StandardCharsets.UTF_8)};
@@ -32,7 +31,7 @@ public class ExprBase64 extends SimpleExpression<String> {
     @SuppressWarnings("unchecked")
     @Override
     public boolean init(Expression<?>[] arg0, int arg1, Kleenean arg2, ParseResult arg3) {
-        b64Ty = arg3.mark;
+        ty = arg3.mark;
         b64 = (Expression<String>) arg0[0];
         return true;
     }
