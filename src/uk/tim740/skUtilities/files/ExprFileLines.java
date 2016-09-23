4a744d12ea9fc455e0d8be7445b25edb640c0856
tim740 <tim740x@gmail.com>
2016-09-23 14:46:15 +0100

    optimize file lines

--- src/uk/tim740/skUtilities/files/ExprFileLines.java
+++ src/uk/tim740/skUtilities/files/ExprFileLines.java
@@ -9,9 +9,10 @@ import uk.tim740.skUtilities.Utils;
 import uk.tim740.skUtilities.skUtilities;
 
 import javax.annotation.Nullable;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Created by tim740 on 17/03/2016
@@ -22,9 +23,9 @@ public class ExprFileLines extends SimpleExpression<Number> {
     @Override
     @Nullable
     protected Number[] get(Event e) {
-        File pth = new File(Utils.getDefaultPath(path.getSingle(e)));
+        Path pth = Paths.get(Utils.getDefaultPath(path.getSingle(e)));
         try {
-            return new Number[]{Files.readAllLines(pth.toPath()).size()};
+            return new Number[]{Files.lines(pth).count()};
         } catch (IOException x) {
             skUtilities.prSysE("File: '" + pth + "' doesn't exist, or is not readable!", getClass().getSimpleName(), x);
         } catch (Exception x) {
