bfb655eee8ecd4da14fa9f21de0d22124bc4c081
tim740 <tim740x@gmail.com>
2016-10-15 16:57:20 +0100

    urlSSLSerialNumber

--- src/uk/tim740/skUtilities/RegUrl.java
+++ src/uk/tim740/skUtilities/RegUrl.java
@@ -15,9 +15,10 @@ class RegUrl {
         Skript.registerExpression(ExprUrlSize.class, String.class, ExpressionType.PROPERTY, "size of url %string%", "url %string%'s size");
         Skript.registerExpression(ExprUrlSizeBytes.class, Number.class, ExpressionType.PROPERTY, "size of url %string% in bytes", "url %string%'s size in bytes");
         Skript.registerExpression(ExprUrlResponseCode.class, Integer.class, ExpressionType.PROPERTY, "response code of url %string%", "url %string%'s response code");
+        Skript.registerExpression(ExprUrlLastModified.class, Number.class, ExpressionType.PROPERTY, "last modified value of url %string%", "url %string%'s last modified value");
+
         Skript.registerExpression(ExprUrlSSLVerifier.class, String.class, ExpressionType.PROPERTY, "ssl verifier of url %string%", "url %string%'s ssl verifier");
         Skript.registerExpression(ExprUrlSSLSerialNumber.class, String.class, ExpressionType.PROPERTY, "ssl serial number of url %string%", "url %string%'s ssl serial number");
-        Skript.registerExpression(ExprUrlLastModified.class, Number.class, ExpressionType.PROPERTY, "last modified value of url %string%", "url %string%'s last modified value");
         Skript.registerExpression(ExprUrlSSLIssueExpire.class, Number.class, ExpressionType.PROPERTY, "ssl (0¦issue|1¦expire) value of url %string%", "url %string%'s ssl (0¦issue|1¦expire) value");
     }
 }
