c02605b44324789636419ce83e6ee5e4f65f0f59
tim740 <tim740x@gmail.com>
2016-10-15 15:41:49 +0100

    urlSSLVerifier

--- src/uk/tim740/skUtilities/RegUrl.java
+++ src/uk/tim740/skUtilities/RegUrl.java
@@ -16,5 +16,6 @@ class RegUrl {
         Skript.registerExpression(ExprUrlSizeBytes.class, Number.class, ExpressionType.PROPERTY, "size of url %string% in bytes", "url %string%'s size in bytes");
         Skript.registerExpression(ExprUrlResponseCode.class, Integer.class, ExpressionType.PROPERTY, "response code of url %string%", "url %string%'s response code");
         Skript.registerExpression(ExprUrlLastModified.class, Number.class, ExpressionType.PROPERTY, "last modified value of url %string%", "url %string%'s last modified value");
+        Skript.registerExpression(ExprUrlSSLVerifier.class, String.class, ExpressionType.PROPERTY, "ssl verifier of url %string%", "url %string%'s ssl verifier");
     }
 }
---
+++ src/uk/tim740/skUtilities/url/ExprUrlSSLVerifier.java
@@ -0,0 +1,68 @@
+package uk.tim740.skUtilities.url;
+
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import uk.tim740.skUtilities.skUtilities;
+
+import javax.annotation.Nullable;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.URL;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+
+/**
+ * Created by tim740 on 15/10/2016
+ */
+public class ExprUrlSSLVerifier extends SimpleExpression<String> {
+    private Expression<String> url;
+
+    @Override
+    @Nullable
+    protected String[] get(Event e) {
+        try {
+            HttpsURLConnection c = (HttpsURLConnection) new URL(url.getSingle(e)).openConnection();
+            c.connect();
+            Certificate[] certs = c.getServerCertificates();
+            for (Certificate cert : certs) {
+                if (cert instanceof X509Certificate) {
+                    X509Certificate sc = (X509Certificate ) cert;
+                    c.disconnect();
+                    String[] s = sc.getIssuerX500Principal().getName().split("O=");
+                    s = s[1].split(",C");
+                    return new String[]{s[0]};
+                }
+            }
+        } catch (IOException x) {
+            skUtilities.prSysE("Error Reading from: '" + url.getSingle(e) + "' Is the site down?", getClass().getSimpleName(), x);
+        } catch (Exception x) {
+            skUtilities.prSysE(x.getMessage(), getClass().getSimpleName(), x);
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(Expression<?>[] e, int i, Kleenean k, ParseResult p) {
+        url = (Expression<String>) e[0];
+        return true;
+    }
+
+    @Override
+    public Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean b) {
+        return getClass().getName();
+    }
+}
